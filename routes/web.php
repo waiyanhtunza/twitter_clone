d6395e0a849c3599224e7814743e00337e2f146a
waiyanhtunza <wyh@za.com.mm>
2024-01-24 10:34:19 +0630

    group route to resource

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,33 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
+use App\Models\Idea;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/terms', [DashboardController::class, 'terms'])->name('terms');
 
-Route::group(['prefix' => '', 'as' => 'ideas.'], function () {
-    Route::post('/ideas', [IdeaController::class, 'store'])->name('store');
+// Route::group(['prefix' => '', 'as' => 'ideas.'], function () {
+//     Route::get('{idea}/show', [IdeaController::class, 'show'])->name('show');
 
-    Route::get('{idea}/show', [IdeaController::class, 'show'])->name('show');
+//     Route::group(['middleware' => ['auth']], function () {
+   
+//         Route::post('/ideas', [IdeaController::class, 'store'])->name('store');
 
-    Route::group(['middleware' => ['auth']], function () {
-        Route::delete('{idea}', [IdeaController::class, 'destroy'])->name('destroy');
+//         Route::delete('{idea}', [IdeaController::class, 'destroy'])->name('destroy');
 
-        Route::get('{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
+//         Route::get('{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
 
-        Route::put('{idea}', [IdeaController::class, 'update'])->name('update');
+//         Route::put('{idea}', [IdeaController::class, 'update'])->name('update');
 
-        Route::post('{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
-    });
-});
+//         Route::post('{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
+//     });
+// });
+
+Route::resource('ideas', IdeaController::class)
+    ->except('index', 'create', 'show')
+    ->middleware('auth');
+
+Route::resource('ideas', IdeaController::class)->only('show');
+
+Route::resource('ideas.comments', CommentController::class)->only('store')->middleware('auth');
